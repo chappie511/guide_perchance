a244e47877990709eeef01218863f3394d437b47
Chappie <justindion7@gmail.com>
2026-09-11 22:15:26 -0400

    Update APP_VERSION to 'guide-perchance-v1.4'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Numéro de version unique pour votre projet
-const APP_VERSION = 'guide-perchance-v1.3';
+const APP_VERSION = 'guide-perchance-v1.4';
 
 // Fonction pour charger et injecter du HTML de manière dynamique
 function chargerSection(idDeLaBoite, cheminDuFichier) {
