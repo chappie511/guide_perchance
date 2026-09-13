3241e583aa88fe1513d48993fda3200277122f61
Chappie <justindion7@gmail.com>
2026-09-13 19:15:21 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -163,7 +163,7 @@ if (reloadBtn) {
 function afficherVersion() {
   const versionSpan = document.getElementById('app-version');
   if (versionSpan) {
-    versionSpan.textContent = 'v1.3.0';
+    versionSpan.textContent = 'v1.3.1';
   }
 }
 
