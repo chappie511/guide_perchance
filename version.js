56c58850b04ea5421bc328dee951e06bfb279f32
Chappie <justindion7@gmail.com>
2026-09-21 15:18:09 -0400

    Add files via upload

---
+++ version.js
@@ -0,0 +1,20 @@
+// 1. Tu inventes le nom de la constante globale
+const APP_VERSION = 'v1.3.8';
+console.log(`Version : ${APP_VERSION}`);
+
+// 2. Tu inventes le nom de la fonction
+function injecterMaVersion() {
+  // Tu inventes le nom de la variable locale (ex: monElement)
+  const monElement =  document.getElementById('app-version');
+  
+  if (monElement) {
+    monElement.textContent = APP_VERSION;
+  }
+}
+
+// 3. Gestion du chargement (Les mots en bleu sont FIXES et imposés par JavaScript)
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', injecterMaVersion);
+} else {
+  injecterMaVersion();
+}
