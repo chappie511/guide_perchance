7eed55357e023eb0a1f1cab4fdeea7a19a9820b1
Chappie <justindion7@gmail.com>
2026-09-21 18:41:22 -0400

    Update version.js

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // 1. Tu inventes le nom de la constante globale
-const APP_VERSION = 'v1.4.2';
+const APP_VERSION = 'v1.4.3';
 console.log(`Version : ${APP_VERSION}`);
 
 // 2. Tu inventes le nom de la fonction
